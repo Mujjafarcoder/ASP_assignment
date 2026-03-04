03219f7e4bde7c01dabd599437cb42c8841aa5b2
Mujjafar <mujafarbeg27@gmail.com>
2026-03-04 09:29:13 +0530

    fix vercel build errors

--- src/Components/Headpart.js
+++ src/Components/Headpart.js
@@ -1,4 +1,4 @@
-  import React, { useState } from 'react'
+  import React from 'react'
   import '../Styles/applayout.css'
    import '../index.css'
 
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../Styles/login.css'
-import { Navigate, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 export default function Login() {
 
     const [firstName, setFirstName] = useState("");
